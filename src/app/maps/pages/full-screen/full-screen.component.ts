86176fdd2de46d8f025e55b08bb9e076adae170b
ivanretamozo <retamozoi445@gmail.com>
2022-08-04 13:38:01 -0300

    zoom y pantalla de zoomrange

--- src/app/maps/pages/full-screen/full-screen.component.ts
+++ src/app/maps/pages/full-screen/full-screen.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
-import { environment } from '../../../../environments/environment';
 
 @Component({
     selector: 'app-full-screen',
@@ -11,10 +10,11 @@ export class FullScreenComponent implements OnInit {
     constructor() {}
 
     ngOnInit(): void {
-        (mapboxgl as any).accessToken = environment.mapboxKey;
         var map = new mapboxgl.Map({
             container: 'map',
             style: 'mapbox://styles/mapbox/streets-v11',
+            center: [-74.925, 45.28],
+            zoom: 18,
         });
     }
 }
--- src/app/maps/pages/zoom-range/zoom-range.component.ts
+++ src/app/maps/pages/zoom-range/zoom-range.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 @Component({
-  selector: 'app-zoom-range',
-  templateUrl: './zoom-range.component.html',
-  styleUrls: ['./zoom-range.component.scss']
+    selector: 'app-zoom-range',
+    templateUrl: './zoom-range.component.html',
+    styleUrls: ['./zoom-range.component.scss'],
 })
-export class ZoomRangeComponent implements OnInit {
+export class ZoomRangeComponent implements AfterViewInit {
+    @ViewChild('divMap') divMap!: ElementRef;
 
-  constructor() { }
+    map!: mapboxgl.Map;
 
-  ngOnInit(): void {
-  }
+    constructor() {}
 
+    ngAfterViewInit(): void {
+        this.map = new mapboxgl.Map({
+            container: this.divMap.nativeElement,
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: [-74.925, 45.28],
+            zoom: 18,
+        });
+    }
+
+    zoomIn() {
+        this.map.zoomIn();
+    }
+
+    zoomOut() {
+        this.map.zoomOut();
+    }
 }
